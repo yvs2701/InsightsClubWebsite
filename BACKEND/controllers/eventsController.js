b99f6254ddcde0a0bdc6bd3cd40f0a407feadb5c
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-23 23:05:13 +0530

    event model updated and cloudinary integrated

--- BACKEND/controllers/eventsController.js
+++ BACKEND/controllers/eventsController.js
@@ -1,6 +1,14 @@
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const Events = require("../models/eventsModel");
 const ErrorHandler = require("../utils/errorHandler");
+const cloudinary = require("cloudinary").v2;
+const dotenv = require("dotenv");
+dotenv.config({ path: "config/config.env" });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 exports.homePage = catchAsyncErrors(async (req, res, next) => {
     res.send("Let's go");
@@ -25,7 +33,29 @@ exports.getAllEvents = catchAsyncErrors(async (req, res, next) => {
 //* CREATE NEW EVENT
 exports.createNewEvent = catchAsyncErrors(async (req, res, next) => {
     
-    const event = await Events.create(req.body);
+    const img = req.files.image;
+
+    if (!img) {
+        return next(new ErrorHandler("Image not available", 404));
+    }
+
+    const myCloud = await cloudinary.uploader.upload(img.tempFilePath, {
+      folder: "Events",
+    });
+
+    const { title, description, date, domain, department } = req.body;
+
+    const event = await Events.create({
+        title,
+        description,
+        domain, 
+        department,
+        date,
+        image: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+        }
+    });
     res.status(201).json({
         success: true,
         event
