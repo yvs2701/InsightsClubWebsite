2eba3d903f14d7e4c9b62cf5d6189025a5e52726
Aditya Bhate <adityabhate305@gmail.com>
2022-08-03 20:17:06 +0530

    blogs boilerplate

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import Landing from "./pages/Landing/Landing";
+import BlogsLanding from "./pages/BlogsLanding/BlogsLanding";
 
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<Navbar />} />
+			<Route path='/' element={<Landing />} />
+			<Route path='/blogs' element={<BlogsLanding />} />
 		</Routes>
 	);
 }
--- FRONTEND/src/components/Navbar/Navbar.jsx
+++ FRONTEND/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
-import Logo from "../../media/logo.png";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getEvents } from "../../actions/events";
+import Logo from "../../media/InsightsLogo.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { getEvents } from "../../actions/events";
 import "./Navbar.css";
 
 function Navbar() {
 	const [menu, setMenu] = useState(false);
-	// const dispatch = useDispatch();
-	// useEffect(() => {
-	// 	dispatch(getEvents());
-	// }, [dispatch]);
-	// const events = useSelector((state) => state.events);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getEvents());
+	}, [dispatch]);
+	const events = useSelector((state) => state.events);
+	console.log(events);
 
 	return (
-		<>
+		<div className='nav-body'>
 			<nav className={menu ? "menu-active" : "navbar"}>
 				<div className='nav-logo'>
 					<img src={Logo} className='logo' alt='logo' />
-					<span className='logo-name'>Insights Club</span>
 				</div>
 				<div className='push-left'>
 					<button
@@ -33,29 +33,49 @@ function Navbar() {
 
 					<ul id='primary-menu' className='menu nav-menu'>
 						<li className='menu-item current-menu-item'>
-							<a className='nav__link' href='/'>
+							<a className='nav_link' href='/'>
 								Home
 							</a>
 						</li>
 						<li className='menu-item dropdown'>
-							<a className='nav__link' href='/events'>
+							<a className='nav_link' href='/events'>
+								News
+							</a>
+						</li>
+						<li className='menu-item dropdown'>
+							<a className='nav_link' href='/events'>
 								Events
 							</a>
 						</li>
 						<li className='menu-item dropdown'>
-							<a className='nav__link' href='/blogs'>
+							<a className='nav_link' href='/blogs'>
 								Blogs
 							</a>
 						</li>
 						<li className='menu-item dropdown'>
-							<a className='nav__link' href='/about'>
+							<a className='nav_link' href='/about'>
 								About
+							</a>
+						</li>
+						<li className='menu-item dropdown'>
+							<a className='nav_link' href='/about'>
+								Contacts
+							</a>
+						</li>
+						<li className='menu-item dropdown'>
+							<a className='nav_link' href='/about'>
+								<button className='navbar-login'>Login</button>
+							</a>
+						</li>
+						<li className='menu-item dropdown'>
+							<a className='nav_link' href='/about'>
+								<button className='navbar-signup'>Sign up</button>
 							</a>
 						</li>
 					</ul>
 				</div>
 			</nav>
-		</>
+		</div>
 	);
 }
 
