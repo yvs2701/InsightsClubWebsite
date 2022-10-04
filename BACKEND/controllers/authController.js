03dd8d62df9f229bbedf587863b5861676ae8119
yvs2701 <yashvardhan.ys86@gmail.com>
2022-10-04 13:22:01 +0530

    Cookie changes - Cookies are now less secure - but will be sent cross site

--- BACKEND/controllers/authController.js
+++ BACKEND/controllers/authController.js
@@ -53,7 +53,7 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
         else try {
             jwt.verify(req.cookies.token, process.env.JWT_SECRET);
             // verified successfully
-            res.setHeader('set-cookie', ['token=loggedout; path=/; samesite=lax; httponly; Secure;', 'user={}; path=/; samesite=lax; Secure;']); // added secure for production
+            res.setHeader('set-cookie', ['token=loggedout; path=/; samesite=none; httponly; Secure;', 'user={}; path=/; samesite=none; Secure;']); // added secure for production
             res.status(200).json({ success: true, message: "User logged out !!" });
         } catch (e) {
             if (e.name == 'TokenExpiredError')
@@ -101,12 +101,12 @@ exports.login = catchAsyncErrors(async (req, res, next) => {
 				payload.name = doc.name;
 				payload.department = doc.department;
 
-                res.setHeader('set-cookie', [`token=${token}; path=/; expires=${expiry_date}; samesite=lax; httponly; Secure;`, `user=${JSON.stringify(payload)}; path=/; expires=${expiry_date}; samesite=lax; Secure;`]);
+                res.setHeader('set-cookie', [`token=${token}; path=/; expires=${expiry_date}; samesite=none; httponly; Secure;`, `user=${JSON.stringify(payload)}; path=/; expires=${expiry_date}; samesite=none; Secure;`]);
 
                 res.status(200).json({ success: true, message: 'User logged in !!' });
             }
             else {
-                res.setHeader('set-cookie', ['token=loggedout; path=/; samesite=lax; httponly; Secure;', 'user={}; path=/; samesite=lax; Secure;']); // added secure for production
+                res.setHeader('set-cookie', ['token=loggedout; path=/; samesite=none; httponly; Secure;', 'user={}; path=/; samesite=none; Secure;']); // added secure for production
 
 				return next(new ErrorHandler("Invalid credentials !!", 400));
 			}
