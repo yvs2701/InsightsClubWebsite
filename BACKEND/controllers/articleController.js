f458097e39654dddd777784a3f10fe5b48a5dbb6
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-11-01 17:38:51 +0530

    :wrench: update article controller and model

--- BACKEND/controllers/articleController.js
+++ BACKEND/controllers/articleController.js
@@ -5,18 +5,6 @@ const Articles = require("../models/articleModel");
 // const dotenv = require("dotenv");
 // dotenv.config({ path: "config/config.env" });
 
-//Function to check if user is the author of the article
-const isAuthor = async (articleID, userID) => {
-    const isValid = mongoose.Types.ObjectId.isValid(articleID);
-    if (!isValid)
-        return next(new ErrorHandler(`${articleID} is not valid !!`, 400));
-
-    const article = await Articles.findById(articleID).populate('author', '-password -verified').exec();
-    if (userID.localeCompare(article.author.id) == 0)
-        return { _isAuthor: true, article };
-    return { _isAuthor: false };
-}
-
 //Get all articles
 exports.getAllArticles = catchAsyncErrors(async (req, res, next) => {
     const articles = await Articles.find({}, {_id: 1, title: 1, description: 1, tags: 1, author: 1}).populate('author', '-password -verified').lean();
@@ -35,34 +23,46 @@ exports.getArticle = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({ success: true, article });
 });
 
-//Create article
+//Create article by admin or co-admin
 exports.createArticle = catchAsyncErrors(async (req, res, next) => {
-    const author = mongoose.Types.ObjectId(req.user.id);
-    const { title, description, tags, content } = req.body;
-    const article = await Articles.create({
-        title,
-        description,
-        tags,
-        content,
-        author
-    });
-    res.status(201).json({ success: true, article });
+    if (req.user.isAdmin || req.user.isCoAdmin) {
+        const lastEditedBy = mongoose.Types.ObjectId(req.user.id);
+        const { title, description, tags, content } = req.body;
+        const article = await Articles.create({
+            title,
+            description,
+            tags,
+            content,
+            lastEditedBy
+        });
+        res.status(201).json({ success: true, article });
+    }
 });
 
-//Update article
+//Update article by admin or co-admin
 exports.updateArticle = catchAsyncErrors(async (req, res, next) => {
-    const isAuthor = await isAuthor(req.params.id, req.user.id);
-    if (!isAuthor._isAuthor)
-        return next(new ErrorHandler(`You are not the author of this article !!`, 403));
-    await Articles.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    res.status(200).json({ success: true, message: "Article updated successfully" });
+    if (req.user.isAdmin || req.user.isCoAdmin) {
+        const lastEditedBy = mongoose.Types.ObjectId(req.user.id);
+        const { title, description, tags, content } = req.body;
+        const article = await Articles.findByIdAndUpdate(req.params.id, {
+            title,
+            description,
+            tags,
+            content,
+            lastEditedBy
+        }, { runValidators: true, new: true });
+        res.status(200).json({ success: true, article });
+    } else {
+        return next(new ErrorHandler(`You are not authorized to perform this action`, 401));
+    }
 });
 
-//Delete article
+//Delete article by admin or co-admin
 exports.deleteArticle = catchAsyncErrors(async (req, res, next) => {
-    const isAuthor = await isAuthor(req.params.id, req.user.id);
-    if (!isAuthor._isAuthor)
-        return next(new ErrorHandler(`You are not the author of this article !!`, 403));
-    await Articles.findByIdAndDelete(req.params.id);
-    res.status(200).json({ success: true, message: "Article deleted successfully" });
+    if (req.user.isAdmin || req.user.isCoAdmin) {
+        const article = await Articles.findByIdAndDelete(req.params.id);
+        res.status(200).json({ success: true, article });
+    } else {
+        return next(new ErrorHandler(`You are not authorized to perform this action`, 401));
+    }
 });
